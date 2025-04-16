dec246bfdf8978774a4f7353e9c7406168ba20cb
mustali17 <mustalichunawala@gmail.com>
2025-04-16 21:18:53 +0530

    refactor: fix subcategory type in ProductSchema to an array

--- models/Product.js
+++ models/Product.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const ProductSchema = new Schema({
-    brandId: { type: Schema.Types.ObjectId, ref: "Brand", required: true },
-    name: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    category: { type: String, required: true },
-    subcategory: { type: String, required: true },
-    description: { type: String },
-    votes: { type: Number, default: 0 },
-  }, { timestamps: true });
+  brandId: { type: Schema.Types.ObjectId, ref: "Brand", required: true },
+  name: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  category: { type: String, required: true },
+  subcategory: [{ type: String, required: true }],
+  description: { type: String },
+  votes: { type: Number, default: 0 },
+}, { timestamps: true });
 
 export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
