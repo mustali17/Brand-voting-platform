b0922f27b3fc94db22c880872f439cecbb4f8c06
Hitesh Cadscripts <hitesh.cadscripts@gmail.com>
2025-05-05 13:20:46 +0530

    feat: implement category management with search functionality and UI components

--- components/LeftSideBar.tsx
+++ components/LeftSideBar.tsx
@@ -27,24 +27,17 @@ const LeftSideBar = () => {
         <nav className='flex-1'>
           <div className='space-y-1 px-3'>
             <Link
-              href='#'
+              href='/'
               className='flex items-center px-3 py-3 text-xl my-4 italic font-medium rounded-md hover:bg-gray-100'
             >
               Brand Voting
             </Link>
             <Link
-              href='#'
+              href='/'
               className='flex items-center px-3 py-3 text-sm font-medium rounded-md hover:bg-gray-100'
             >
               <Home className='mr-3 h-5 w-5' />
               Home
-            </Link>
-            <Link
-              href='#'
-              className='flex items-center px-3 py-3 text-sm font-medium rounded-md hover:bg-gray-100'
-            >
-              <Compass className='mr-3 h-5 w-5' />
-              Explore
             </Link>
 
             <Link
---
+++ components/categoryList.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { CategoryDetailsDto } from '@/utils/models/category.model';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function CategoryList({
+  categories,
+  isLoading,
+  isError,
+  categoryClickHandler,
+}: {
+  categories: any;
+  isLoading: boolean;
+  isError: boolean;
+  categoryClickHandler?: (category: CategoryDetailsDto) => void;
+}) {
+  if (isLoading) {
+    return <div className='flex-1 p-4'>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div className='flex-1 p-4'>Error loading categories</div>;
+  }
+  return (
+    <div className='flex-1 p-4'>
+      <div className='grid grid-cols-4 gap-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8'>
+        {categories.map((category: any) => (
+          <Link
+            key={category._id}
+            href={`#`}
+            onClick={() => {
+              if (categoryClickHandler) {
+                categoryClickHandler(category);
+              }
+            }}
+            className='flex flex-col items-center'
+          >
+            <div className='mb-2 rounded-full overflow-hidden bg-gray-100 p-1'>
+              <Image
+                src={category.categoryImageURL || '/placeholder.svg'}
+                alt={category.name}
+                width={80}
+                height={80}
+                className='h-16 w-16 rounded-full object-cover transition-transform hover:scale-110'
+              />
+            </div>
+            <span className='text-center text-sm'>{category.name}</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
