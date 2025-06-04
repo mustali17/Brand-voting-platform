8a9bf049334287fa9733481410be7b0691e464f9
codebymind110 <92098672+codebymind110@users.noreply.github.com>
2025-06-04 23:22:11 +0530

    fix: standardize quotation marks and improve formatting in RegisterPage component

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,50 +1,50 @@
-'use client';
-import LoadingComponent from '@/components/LoadingComponent';
-import FormComponent from '@/components/ui/FormComponent';
-import { useSendOtpMutation } from '@/lib/services/user.service';
-import { InputFormType } from '@/utils/models/common.model';
-import bcrypt from 'bcryptjs';
-import { signIn, useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { FcGoogle } from 'react-icons/fc';
+"use client";
+import LoadingComponent from "@/components/LoadingComponent";
+import FormComponent from "@/components/ui/FormComponent";
+import { useSendOtpMutation } from "@/lib/services/user.service";
+import { InputFormType } from "@/utils/models/common.model";
+import bcrypt from "bcryptjs";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
 
 const formList: InputFormType[] = [
   {
-    name: 'name',
-    key: 'name',
-    label: 'Full Name',
-    type: 'text',
-    validation: 'nullValidation',
+    name: "name",
+    key: "name",
+    label: "Full Name",
+    type: "text",
+    validation: "nullValidation",
   },
   {
-    name: 'email',
-    key: 'email',
-    label: 'Email',
-    type: 'email',
-    validation: 'emailValidation',
+    name: "email",
+    key: "email",
+    label: "Email",
+    type: "email",
+    validation: "emailValidation",
   },
   {
-    name: 'password',
-    key: 'password',
-    label: 'Password',
-    type: 'password',
-    validation: 'passwordValidation',
+    name: "password",
+    key: "password",
+    label: "Password",
+    type: "password",
+    validation: "passwordValidation",
   },
   {
-    name: 'confirmPassword',
-    key: 'confirmPassword',
-    label: 'Confirm Password',
-    type: 'password',
-    validation: 'passwordValidation',
+    name: "confirmPassword",
+    key: "confirmPassword",
+    label: "Confirm Password",
+    type: "password",
+    validation: "passwordValidation",
   },
 
   {
-    name: 'agree',
-    key: 'agree',
-    label: 'Accept our terms and privacy policy',
-    type: 'checkbox',
+    name: "agree",
+    key: "agree",
+    label: "Accept our terms and conditions",
+    type: "checkbox",
   },
 ];
 
@@ -71,51 +71,51 @@ const RegisterPage = () => {
     formState: { errors, isValid },
   } = useForm<SignUpFormType>({
     defaultValues: {
-      name: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
       agree: undefined,
     },
-    mode: 'onSubmit',
+    mode: "onSubmit",
   });
   //#endregion
 
   //#region Internal Hooks
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   useEffect(() => {
-    if (sessionStatus === 'authenticated') {
-      router.replace('/');
+    if (sessionStatus === "authenticated") {
+      router.replace("/");
     }
   }, [sessionStatus, router]);
 
   useEffect(() => {
-    const password = watch('password');
-    const confirmPassword = watch('confirmPassword');
-    const agree = watch('agree');
+    const password = watch("password");
+    const confirmPassword = watch("confirmPassword");
+    const agree = watch("agree");
     if (password && confirmPassword && password !== confirmPassword) {
-      setError('confirmPassword', {
-        type: 'manual',
-        message: 'Passwords do not match',
+      setError("confirmPassword", {
+        type: "manual",
+        message: "Passwords do not match",
       });
     } else {
-      clearErrors('confirmPassword');
+      clearErrors("confirmPassword");
     }
     if (!agree && agree !== undefined) {
-      setError('agree', {
-        type: 'manual',
-        message: 'You must accept the terms and conditions',
+      setError("agree", {
+        type: "manual",
+        message: "You must accept the terms and conditions",
       });
     } else {
-      clearErrors('agree');
+      clearErrors("agree");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [watch('password'), watch('confirmPassword'), watch('agree')]);
+  }, [watch("password"), watch("confirmPassword"), watch("agree")]);
   //#endregion
 
   //#region Internal Function
   const onSubmit = async (dto: SignUpFormType) => {
-    console.log('dto', dto);
+    console.log("dto", dto);
 
     if (dto.email) {
       const saltRounds = 10;
@@ -125,8 +125,8 @@ const RegisterPage = () => {
       delete dto.agree;
       const data = await sendOtp({ email: dto.email }).unwrap();
       if (data.success) {
-        localStorage.setItem('signUpData', JSON.stringify(dto));
-        router.push('/verify-email');
+        localStorage.setItem("signUpData", JSON.stringify(dto));
+        router.push("/verify-email");
       }
       if (data.error) {
         setErrorMessage(data.error);
@@ -136,12 +136,12 @@ const RegisterPage = () => {
   //#endregion
 
   //#region UI Component
-  if (sessionStatus === 'loading') {
+  if (sessionStatus === "loading") {
     return <LoadingComponent />;
   }
   //#endregion
   return (
-    sessionStatus !== 'authenticated' && (
+    sessionStatus !== "authenticated" && (
       <div className='flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8'>
         <div className='flex justify-center flex-col items-center'>
           <h2 className='lg:mt-6 sm:mt-2 text-center text-2xl leading-9 tracking-tight text-gray-900'>
@@ -182,7 +182,7 @@ const RegisterPage = () => {
               <div className='mt-6'>
                 <button
                   onClick={() => {
-                    signIn('google');
+                    signIn("google");
                   }}
                   className='flex w-full items-center border border-gray-300 justify-center gap-3 rounded-md bg-white px-3 py-1.5 text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
                 >
